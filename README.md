9897e1c9bb2b0de29184a85722707c1217d584b9
CodeBustler <codebustler360online@gmail.com>
2023-06-09 07:25:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# type-conversion
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-epu7jf)
---
+++ script.js
@@ -0,0 +1,10 @@
+let a = '50';
+let b = 10;
+let c = a + b; // Implicit Type conversion
+console.log(typeof c);
+
+let x = '4';
+x = Number.parseInt(x); // explicit type conversion
+let y = 5;
+let z = x + y;
+console.log(typeof z);
